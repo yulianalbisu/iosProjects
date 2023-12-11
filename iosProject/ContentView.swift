4cd46f5a9f7a17d6a89e6658c6557f1a67a5cf58
yuliana <ya.aldrich@yahoo.com>
2023-12-11 18:33:56 -0500

    Initial Commit

---
+++ iosProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosProject
+//
+//  Created by yuliana aldrich on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iosProject/iosProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosProjectApp.swift
+//  iosProject
+//
+//  Created by yuliana aldrich on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
